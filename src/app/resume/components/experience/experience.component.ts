050ad8e75f4553f0484a3d38f25775ee4ca91466
Andres2D <andres2d1997.1206@gmail.com>
2022-01-16 16:11:43 -0500

    migrate from css to scss

--- src/app/resume/components/experience/experience.component.ts
+++ src/app/resume/components/experience/experience.component.ts
@@ -4,7 +4,7 @@ import { WorkExperience } from '../../../interfaces/work-experience.interface';
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.css']
+  styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
 
--- src/app/resume/components/skills/skills.component.ts
+++ src/app/resume/components/skills/skills.component.ts
@@ -4,7 +4,7 @@ import { Skill } from '../../../interfaces/skills.interface';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
 
