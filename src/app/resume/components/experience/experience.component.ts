7f6fbc3801a36a6856ccce5b55c8adaf35b6d878
Andres2D <andres2d1997.1206@gmail.com>
2022-01-23 17:11:27 -0500

    update work experience styles

--- src/app/resume/components/experience/experience.component.ts
+++ src/app/resume/components/experience/experience.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { WorkExperience } from '../../../interfaces/work-experience.interface';
 
 @Component({
@@ -6,7 +6,7 @@ import { WorkExperience } from '../../../interfaces/work-experience.interface';
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss']
 })
-export class ExperienceComponent implements OnInit {
+export class ExperienceComponent {
 
   works: WorkExperience[] = [
     {
@@ -14,27 +14,29 @@ export class ExperienceComponent implements OnInit {
       roleName: 'Frontend Developer',
       from: 2021,
       to: null,
-      description: 'Techs solutions'
+      description: 'Techs solutions',
+      link: 'https://www.wolox.com.ar/'
     },
     {
       workPlace: 'NumRot',
       roleName: 'Web Analist',
       from: 2019,
       to: 2021,
-      description: 'Electronic billing provider'
+      description: 'Electronic billing provider',
+      link: 'https://www.numrot.com/'
     },
     {
       workPlace: 'MotoFox',
       roleName: 'Administrative',
       from: 2015,
       to: 2018,
-      description: 'Familiar business'
+      description: 'Familiar business',
+      link: 'https://motofox-belen.netlify.app/'
     }
   ];
 
-  constructor() { }
-
-  ngOnInit() {
+  goToPage(url: string): void {
+    console.log('in');
+    window.open(url, '_blank');
   }
-
 }
