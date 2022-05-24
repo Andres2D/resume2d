8ca8f150c173c75c951d416f162097a178e0c051
Andres2D <andres2d1997.1206@gmail.com>
2022-05-24 18:55:27 -0500

    Fix full hegiht dinamically in responsives views

--- src/app/resume/components/intro/intro.component.ts
+++ src/app/resume/components/intro/intro.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss']
 })
-export class IntroComponent {
+export class IntroComponent implements OnInit {
+
+  @HostListener('window:resize', ['$event'])
+  onResize({target}: any) {
+    this.defineHeight(target.innerHeight);
+  }
+
+  ngOnInit(): void {
+    this.defineHeight(window.innerHeight);
+  }
 
   goToAbout(): void {
     let el = document.getElementById('about');
     el?.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
+
+  defineHeight(value: number): void {
+    document.documentElement.style.setProperty('--doc-height', `${value}px`);
   }
 }
